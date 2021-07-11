5f7a55d544886ad3f210bd00db93e92de2df7766
why <why@atguigu.com>
2021-07-11 22:20:06 +0800

    push commit1

--- src/main/java/com/atwhy/git/GitTest.java
+++ src/main/java/com/atwhy/git/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
         System.out.println("hellow git3");
         System.out.println("hot-fix test");
         System.out.println("marste test");
-        System.out.println("push test");
+        System.out.println("push test!");
 
     }
 }
