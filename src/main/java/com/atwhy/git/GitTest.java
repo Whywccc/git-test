ea1fce83d13cb763404560c3928dd483df6d83b3
Whywccc <82751459+Whywccc@users.noreply.github.com>
2021-07-11 22:38:07 +0800

    pull commit

--- src/main/java/com/atwhy/git/GitTest.java
+++ src/main/java/com/atwhy/git/GitTest.java
@@ -8,6 +8,8 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("marste test");
         System.out.println("push test!");
+        System.out.println("pull test!");
+        
 
     }
 }
